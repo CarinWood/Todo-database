c778d6d951744275662f7c2027492487a6c51d01
CarinWood <carin.wood.85@gmail.com>
2022-03-30 22:38:56 +0200

    fix with frontend input fields

--- frontend/src/components/inputArea/InputArea.jsx
+++ frontend/src/components/inputArea/InputArea.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import {useState} from 'react'
 import './inputArea.css'
+import { IoClose } from "react-icons/io5";
 
 const InputArea = () => {
+
+  const [task, setTask] = useState('')
+  const [name, setName] = useState('')
+
+  function clearField() {
+    setTask('')
+  }
+
+
+  function clearName() {
+    setName('')
+  }
+
   return (
     <div className='input-fields'>
-       <input className='task-input-field' placeholder="Things you want to do today?" type="text" />
-       <input className='name-input-field' placeholder="Your name" type="text" />
+       <input 
+          className='task-input-field' 
+          placeholder="Things you want to do today?" 
+          type="text"
+          value={task}
+          onChange={e => setTask(e.target.value)}
+        />
+        {task.length > 0 && <IoClose className='delete-input-text' onClick={clearField}/>}
+
+
+       <input 
+          className='name-input-field' 
+          placeholder="Your name" type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+       />
+        {name.length > 0 && <IoClose className='delete-input-name' onClick={clearName}/>}
+
+
        <button className='add-btn'>Add</button>
     </div>
   )
